dcfabcf7cb82db884220d6e9be02f5718a3369e7
akshit kapil <160398005+zoro2024@users.noreply.github.com>
2024-07-14 04:08:19 +0530

    Update Notification.groovy

--- src/org/generic/Notification.groovy
+++ src/org/generic/Notification.groovy
@@ -1,4 +1,6 @@
-def sendSlackNotification() {
+package org.generic
+
+def call() {
     slackSend(
         channel: 'build-status',
         message: "Find Status of Pipeline: ${currentBuild.currentResult} ${env.JOB_NAME} ${env.BUILD_NUMBER} ${BUILD_URL}"
