971783b401a690c16fff8e2bef9fe22e2034c9bf
akshit kapil <160398005+zoro2024@users.noreply.github.com>
2024-07-15 04:23:56 +0530

    Update Trivyinstaller.groovy

--- src/org/generic/Trivyinstaller.groovy
+++ src/org/generic/Trivyinstaller.groovy
@@ -1,5 +1,5 @@
 package org.generic
 
-def call(String repoUrl) {
-    sh "trivy repo ${repoUrl} --format json -o trivy-license-report.json"
+def call(String url) {
+    sh "trivy repo ${url} --format json -o trivy-license-report.json"
 }
