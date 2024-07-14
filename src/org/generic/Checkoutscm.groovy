2abb71ee9c48f8f2c287875ed93c9dbfed04ce61
akshit kapil <160398005+zoro2024@users.noreply.github.com>
2024-07-15 03:27:27 +0530

    Update Checkoutscm.groovy

--- src/org/generic/Checkoutscm.groovy
+++ src/org/generic/Checkoutscm.groovy
@@ -4,7 +4,7 @@ def call(String url, String creds, String branch) {
     stage('Clone') {
         script {
             echo "Cloning repo ${url} from branch ${branch} using creds ${creds}";
-            git branch: "${branch}", credentialsId: "${creds}", url: "${url}"
+            git url: url, branch: branch, credentialsId: creds
         }
     }
 }
