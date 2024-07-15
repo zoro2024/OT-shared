90dd0697c70b0afedc1b5bd317b6c4a7af8c40a7
akshit kapil <160398005+zoro2024@users.noreply.github.com>
2024-07-16 04:27:11 +0530

    Update generic.groovy

--- vars/generic.groovy
+++ vars/generic.groovy
@@ -1,24 +1,24 @@
 import org.generic.Checkout
-// import org.generic.Gitleaks
-// import org.generic.Trivyinstaller
-// import org.generic.Notification
+import org.generic.Gitleaks
+import org.generic.Trivyinstaller
+import org.generic.Notification
 import org.generic.Buildami
 
 def checkout(String url, String creds, String branch = 'main') {
     new Checkout().call(url, creds, branch)
 }
 
-//  def gitleaks() {
-//      new Gitleaks().call()
-//  }
+ def gitleaks() {
+     new Gitleaks().call()
+ }
 
-// def trivyinstaller(String repoUrl) {
-//      new Trivyinstaller().call(url)
-//  }
+def trivyinstaller(String repoUrl) {
+     new Trivyinstaller().call(url)
+ }
 
-// def notification() {
-//     new Notification().call()
-// }
+def notification() {
+    new Notification().call()
+}
 
 def buildami(String ami_name) {
     new Buildami().call(ami_name)
