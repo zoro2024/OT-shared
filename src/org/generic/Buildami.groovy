75637c4c5d898ff6b5c53d784b7d985a790e6123
akshit kapil <160398005+zoro2024@users.noreply.github.com>
2024-07-16 03:41:03 +0530

    Update Buildami.groovy

--- src/org/generic/Buildami.groovy
+++ src/org/generic/Buildami.groovy
@@ -1,10 +1,8 @@
 package org.generic
 
 def call() {
-    stage('Build ami') {
-      script {
-        sh "packer init packer.pkr.hcl"
-        sh "packer build packer.pkr.hcl"
-      }    
-    }
+      sh '''
+        packer init packer.pkr.hcl"
+        packer build packer.pkr.hcl"
+        '''
 }
