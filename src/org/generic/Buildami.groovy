aae7093d6dd89fbce9758efe1542737e804cd084
akshit kapil <160398005+zoro2024@users.noreply.github.com>
2024-07-16 04:07:44 +0530

    Update Buildami.groovy

--- src/org/generic/Buildami.groovy
+++ src/org/generic/Buildami.groovy
@@ -3,6 +3,6 @@ package org.generic
 def call(String ami_name) {
     sh """
         packer init packer.pkr.hcl
-        packer build - var ${ami_name} packer.pkr.hcl
+        packer build -var 'ami_name=${ami_name}' packer.pkr.hcl
     """
 }
