c79d27b06f0a49f996ce889f98be14851913cdb0
akshit kapil <160398005+zoro2024@users.noreply.github.com>
2024-07-16 04:50:32 +0530

    Update Trivyinstaller.groovy

--- src/org/generic/Trivyinstaller.groovy
+++ src/org/generic/Trivyinstaller.groovy
@@ -1,6 +1,6 @@
 package org.generic
 
-def call(String url) {
+def call(String url) {   //github-token is git token and stored in jenkins as secret text
     withCredentials([string(credentialsId: 'github-token', variable: 'GITHUB_TOKEN')]) {
         sh "trivy repo ${url} --format json -o trivy-license-report.json"
     }
