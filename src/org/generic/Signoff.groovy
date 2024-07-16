8f4c49bb9535331c0f62868b11c0ca7ad1dae51b
akshit kapil <160398005+zoro2024@users.noreply.github.com>
2024-07-17 00:01:12 +0530

    Update Signoff.groovy

--- src/org/generic/Signoff.groovy
+++ src/org/generic/Signoff.groovy
@@ -1,13 +1,13 @@
 package org.generic
 
 
-def call(String useremail, String username, String creds, String pushurl, String branch) {
+def call(String useremail, String username, String creds, String url, String branch) {
     stage('configuring git') {
         withCredentials([usernamePassword(credentialsId: creds, passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
             sh """
             git config user.email '${useremail}'
             git config user.name '${username}'
-            git remote set-url origin https://${username}:${PASSWORD}@${pushurl}
+            git remote set-url origin https://${username}:${PASSWORD}@github.com/mygurkulam-p9/Attendence-API.git
             git add CredScanReport
             git commit --signoff -m "Cred report"
             git push origin ${branch}
