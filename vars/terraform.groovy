b199e2dc24d8b5dc05131fe9da20d55e5ef648c8
akshit kapil <160398005+zoro2024@users.noreply.github.com>
2024-08-04 04:20:12 +0530

    Update terraform.groovy

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -1,5 +1,10 @@
 import org.terraform.Terraforminit
+import org.terraform.Terraforminit
 
 def terraforminit(String path) {
     new Terraforminit().call(path)
+}
+
+def terraformformat(String path) {
+    new Terraformformat().call(path)
 }
